0f05e3a9e9897f5709fbe20382e1c48868ff647d
Tonje Stensen <stensentonje@gmail.com>
2022-06-05 14:25:14 +0200

    image error

--- js/script.js
+++ js/script.js
@@ -1,32 +1,18 @@
-const resultsWrapper = document.querySelector(".results");
+const container = document.querySelector(".container");
 
 
-const url = "https://www.superheroapi.com/api.php/1214716615946231/";
+const url = "https://www.superheroapi.com/api.php/1214716615946231/69";
 
-async function callApi(id) {
-
-    console.log(url+id);
-    const response = await fetch(url+id);
+async function callApi() {
+    const response = await fetch(url);
     const json = await response.json();
 
     console.log(json);
 
-    for(var property in json.image) {
-        console.log(json.image[property])
-        resultsWrapper.innerHTML += `<div class="image" style="background-image: url(${json.image[property]});"></div>`
-    }
-
-    for(var property in json.appearance) {
-        console.log(json.appearance[property])
-        resultsWrapper.innerHTML += `<div class="result">${json.appearance[property]}</div>`;
-    }
-
-    for(var property in json.powerstats) {
-        console.log(json.powerstats[property])
-        resultsWrapper.innerHTML += `<div class="result">${json.powerstats[property]}</div>`;
-    }
+    container.innerHTML = `<h1>${json.name}</h1>
+                            <div class="details-image" style="background-image: url('${json.image}')"></div>`;                      
 }
+   
 
-callApi(69);
-callApi(720);
-callApi(644);
+
+callApi();
